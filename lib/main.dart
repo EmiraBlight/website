662a8e455776e004578021ccb4ac866eb84f8e67
Sammy <sam.thorne00@gmail.com>
2026-02-24 21:10:53 -0800

    added game

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'wordle.dart';
 import 'encryption.dart';
 import 'projects.dart';
 import 'contact.dart';
+import 'game.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -41,6 +43,7 @@ class _MainPageState extends State<MainPage> {
     "Contact",
     "Projects",
     "Encryption",
+    "Game",
   ];
 
   @override
@@ -50,60 +53,73 @@ class _MainPageState extends State<MainPage> {
         preferredSize: const Size.fromHeight(70),
         child: Container(
           color: Colors.white,
-          child: Center(
+          child: SafeArea(
             child: LayoutBuilder(
               builder: (context, constraints) {
                 const double tabWidth = 120.0;
+                final double totalWidth = tabWidth * _tabs.length;
 
-                return SizedBox(
-                  width: tabWidth * _tabs.length,
-                  height: 50,
-                  child: Stack(
-                    alignment: Alignment.bottomLeft,
-                    children: [
-                      // Sliding underline
-                      AnimatedPositioned(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut,
-                        left: _selectedIndex * tabWidth,
-                        bottom: 0,
-                        child: Container(
-                          width: tabWidth,
-                          height: 3,
-                          color: Colors.deepPurple,
-                        ),
-                      ),
-                      Row(
-                        children: List.generate(_tabs.length, (index) {
-                          final bool isSelected = _selectedIndex == index;
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _selectedIndex = index;
-                              });
-                            },
-                            child: SizedBox(
-                              width: tabWidth,
-                              height: 50,
-                              child: Center(
-                                child: Text(
-                                  _tabs[index],
-                                  style: TextStyle(
-                                    color: isSelected
-                                        ? Colors.deepPurple
-                                        : Colors.black54,
-                                    fontWeight: isSelected
-                                        ? FontWeight.bold
-                                        : FontWeight.normal,
-                                    fontSize: 16,
-                                  ),
-                                ),
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  child: Container(
+                    // This forces the container to be at least as wide as the screen
+                    // to allow the Center widget to actually center the content.
+                    constraints: BoxConstraints(minWidth: constraints.maxWidth),
+                    child: Center(
+                      child: SizedBox(
+                        width: totalWidth,
+                        height: 50,
+                        child: Stack(
+                          alignment: Alignment.bottomLeft,
+                          children: [
+                            // Sliding underline
+                            AnimatedPositioned(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                              left: _selectedIndex * tabWidth,
+                              bottom: 0,
+                              child: Container(
+                                width: tabWidth,
+                                height: 3,
+                                color: Colors.deepPurple,
                               ),
                             ),
-                          );
-                        }),
+                            Row(
+                              children: List.generate(_tabs.length, (index) {
+                                final bool isSelected = _selectedIndex == index;
+                                return GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      _selectedIndex = index;
+                                    });
+                                  },
+                                  child: Container(
+                                    width: tabWidth,
+                                    height: 50,
+                                    color: Colors.transparent, // Improves tap area
+                                    child: Center(
+                                      child: Text(
+                                        _tabs[index],
+                                        style: TextStyle(
+                                          color: isSelected
+                                              ? Colors.deepPurple
+                                              : Colors.black54,
+                                          fontWeight: isSelected
+                                              ? FontWeight.bold
+                                              : FontWeight.normal,
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }),
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
+                    ),
                   ),
                 );
               },
@@ -119,6 +135,7 @@ class _MainPageState extends State<MainPage> {
           ContactPage(),
           ProjectsPage(),
           EncryptPage(),
+          WebSocketGamePage(),
         ],
       ),
     );
